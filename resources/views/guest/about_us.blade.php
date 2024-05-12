18bb3b1f10d54fba47b2ac76727f4a5794033bb2
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-05-12 21:20:21 +0200

    edit about, add lorem for content and image

--- resources/views/guest/about_us.blade.php
+++ resources/views/guest/about_us.blade.php
@@ -1,5 +1,16 @@
 @extends('layout.app')
 
 @section('main-content')
-    <h1>About Us</h1>
+    <div class="container">
+        <h1>About Us</h1>
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores fuga quis voluptate unde quae iure
+            aspernatur ratione perspiciatis! Accusamus ipsa deleniti accusantium esse recusandae! Similique quas perferendis
+            atque porro cumque dicta ratione earum fugiat, id maxime ipsa itaque. Dolorum possimus, quisquam esse unde ipsam
+            amet perspiciatis laboriosam ipsa qui magnam enim dolore rem! Impedit repudiandae minima corporis similique vel
+            culpa ex sunt porro in, laboriosam tenetur eum laudantium optio veniam officiis mollitia, iste cumque quaerat
+            quos quasi eligendi. Sint repellendus, delectus veritatis ullam voluptates nulla quae vero expedita, unde, illum
+            officiis debitis blanditiis explicabo? Veniam alias fugiat quos dignissimos voluptatem?</p>
+        <img src="" alt="">
+    </div>
 @endsection
