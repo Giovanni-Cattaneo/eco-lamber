384677774d3539e25c1ff7297b65608fc0fa8c6e
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-05-12 16:57:19 +0200

    edit width for p in jumbotron

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -5,7 +5,7 @@
         <div class="p-5 mb-4 bg-transparent rounded-3">
             <div class="container-fluid py-5">
                 <h1 class="display-5 fw-bold text-light">Progetto Eco-Lamber</h1>
-                <p class="col-md-8 fs-4 text-light">
+                <p class="col-md-8 fs-4 text-light w-50">
                     Benvenuto sul nostro sito, cerca informazioni in merito al progetto di solidarietà europea Eco-Lamber,
                     una serie di incontri e di lavori pratici per la sensibilizzazione ecologica sul territorio di carate
                     brianza e frazioni che culminerà con il nostro Eco-Festival del 22 e 23 giugno, non mancate
